bc34b39b647193566470c410ac7e84d3f0ab1aad
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-20 13:56:05 +0100

    properly formed response

---
+++ src/main/scala/com/gu/microserviceWorkshop/Lambda/APIResponse.scala
@@ -0,0 +1,25 @@
+package com.gu.microserviceWorkshop
+
+import io.circe.Encoder
+import io.circe.syntax._
+import io.circe.generic.extras.semiauto._
+
+
+case class APIResponse(statusCode: Int, headers: Map[String, String], body: String)
+
+
+object APIResponse {
+
+  private val stringAPIResponseEncoder : Encoder[APIResponse] = deriveEncoder
+
+  //needed as API gateway requires that the body is returned as a string
+  implicit def responseEncoder: Encoder[APIResponse] = Encoder.instance { responseA =>
+    responseA.copy(body = responseA.body.asJson.noSpaces).asJson(stringAPIResponseEncoder)
+  }
+
+  def fromResult(body: String): APIResponse = {
+      APIResponse(200,  Map("Content-Type" -> "application/json"), body)
+  }
+
+
+}
--- src/main/scala/com/gu/microserviceWorkshop/Lambda/Lambda.scala
+++ src/main/scala/com/gu/microserviceWorkshop/Lambda/Lambda.scala
@@ -1,9 +1,18 @@
 package com.gu.microserviceWorkshop
+import io.circe.syntax._
+import java.nio.charset.StandardCharsets.UTF_8
+
+import java.io.{InputStream, OutputStream}
 
 object Lambda {
 
-  def handler(): String = {
-    "hello world "
+  def handler(in: InputStream, out: OutputStream): Unit = {
+
+    val response = APIResponse(200,  Map("Content-Type" -> "application/json"), "hello world ")
+
+    //no spaces converts json to a string
+    out.write(response.asJson.noSpaces.getBytes(UTF_8))
+
   }
 
 }
